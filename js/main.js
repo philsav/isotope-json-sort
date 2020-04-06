7c1f44b6931eecaeac0d70b4ea55bc26044ec0a8
Philip Savarirayan <philip.sav@gmail.com>
2020-04-06 14:52:30 +0530

    with isotope

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
+// external js: isotope.pkgd.js
 
-import './../scss/main.scss'
+// init Isotope
+var $grid = $('.grid').isotope({
+	itemSelector: '.course-desc'
+});
+
+// store filter for each group
+var filters = {};
+
+$('.filters').on('change', function(event) {
+	var $select = $(event.target);
+	// get group key
+	var filterGroup = $select.attr('value-group');
+	// set filter for group
+	filters[filterGroup] = event.target.value;
+	// combine filters
+	var filterValue = concatValues(filters);
+	// set filter for Isotope
+	$grid.isotope({ filter: filterValue });
+});
+
+// flatten object by concatting values
+function concatValues(obj) {
+	var value = '';
+	for (var prop in obj) {
+		value += obj[prop];
+	}
+	return value;
+}
